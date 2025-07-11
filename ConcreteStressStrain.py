0c406705c05229a0e221bf77bd514b13c33de920
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2025-07-11 10:06:33 +0530

    Update ConcreteStressStrain.py

--- ConcreteStressStrain.py
+++ ConcreteStressStrain.py
@@ -1,1 +1,15 @@
 
+def fib_design(fcd,Sc2,Scu2,n=11):
+  """
+  As per Section 31.1.2.1.2 of Model Code 2020"""
+  strain = np.linspace(0,Scu2,n)
+  pass
+
+def HognestadConcreteCompressureBehaviour(fc,Sc,Scu,n=11):
+  strain = np.linspace(0,Scu2,n)
+  stress = np.zeros(11)
+  for i,e in enumerate(strain):
+    if e <= Sc:
+      stress[i] = fc * ( 2*(e/Sc) - (e/Sc)**2 )
+    else:
+      stress[i] = fc - 0
